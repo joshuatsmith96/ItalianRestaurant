813b43ce8a8b7dde8ec62668996ed8ed0d23e311
joshuatsmith96 <joshuatsmith96@gmail.com>
2021-01-31 19:02:35 -0600

    New Additions (read desc for more info)
    Added the following:
    
    -'Reservation' form, as well as error handeling.
    -Fixed resizing issue with Nav bar.
    -Dropdown menu with photos, fully functioning.

--- script.js
+++ script.js
@@ -46,6 +46,57 @@ function outEx(){
     bar[1].style.display = null;
 }
 
+
+function findTable(){
+    var date = document.getElementById("date");
+    var time = document.getElementById("time");
+    var size = document.getElementById("partySize");
+
+    var dateError = document.getElementById("dateError");
+    var timeError = document.getElementById("timeError");
+    var sizeError = document.getElementById("sizeError");
+
+    var success = document.getElementById("resSuccess");
+
+    if (!isNaN(date.value)){
+        dateError.style.visibility = "visible";
+    } else {
+        dateError.style.visibility = "hidden";
+    }
+
+    if (!isNaN(time.value)){
+        timeError.style.visibility = "visible";
+    } else {
+        timeError.style.visibility = "hidden";
+    }
+
+    if (size.value == 0){
+        sizeError.style.visibility = "visible";
+    } else {
+        sizeError.style.visibility = "hidden";
+    }
+
+    if (isNaN(date.value) && isNaN(time.value) && size.value > 0){
+        success.style.display = "block";
+    }
+}
+
+var oc = 0;
+function photoDrop(){
+    var text = document.getElementById("dropText");
+    var container = document.getElementById("imageContainer");
+
+    if (oc == 0){
+        container.style.height = "100%";
+        text.innerHTML = "Click to Close Images &#8593;";
+        oc++;
+    } else if(oc == 1){
+        container.style.height = "0";
+        text.innerHTML = "Click to View Images &#8595;";
+        oc--;
+    }
+}
+
 window.addEventListener('load', (event) => {
 
   });
